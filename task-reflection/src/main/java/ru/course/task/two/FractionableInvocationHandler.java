87cd1e6d23cbfa80e87c58a5e98360149ccf59ee
Tatiana Pastel <pastel.t@yandex.ru>
2024-03-23 19:53:43 +0400

    correction of conditions

--- task-reflection/src/main/java/ru/course/task/two/FractionableInvocationHandler.java
+++ task-reflection/src/main/java/ru/course/task/two/FractionableInvocationHandler.java
@@ -1,35 +1,33 @@
 package ru.course.task.two;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class FractionableInvocationHandler implements InvocationHandler {
-    private final Object obj;
-    private final ConcurrentHashMap<Integer, Object> chmCache = new ConcurrentHashMap<>();
+public class FractionableInvocationHandler<T> implements InvocationHandler {
+    private final T obj;
+    private final ConcurrentHashMap<Method, Object> resultCache = new ConcurrentHashMap<>();
 
-    public FractionableInvocationHandler(Object obj) {
+    public FractionableInvocationHandler(T obj) {
         this.obj = obj;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Object result = null;
+        Object result;
         Method m = obj.getClass().getMethod(method.getName(), method.getParameterTypes());
-        Annotation[] cacheAnnotations = m.getAnnotationsByType(Cache.class);
-        if (cacheAnnotations.length > 0) {
-            if (chmCache.isEmpty()) {
+
+        if (m.isAnnotationPresent(Cache.class)) {
+            if (resultCache.isEmpty()) {
                 result = method.invoke(obj, args);
-                Integer key = 1;
-                chmCache.put(key, result);
+                resultCache.put(method, result);
             } else {
-                result = chmCache.get(1);
+                result = resultCache.get(method);
             }
-        }
-        Annotation[] mutatorAnnotations = m.getAnnotationsByType(Mutator.class);
-        if (mutatorAnnotations.length > 0) {
-            chmCache.clear();
+        } else if (m.isAnnotationPresent(Mutator.class)) {
+            resultCache.clear();
+            result = method.invoke(obj, args);
+        } else {
             result = method.invoke(obj, args);
         }
         return result;
